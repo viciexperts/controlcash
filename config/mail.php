dae71b2c475aaf6f2ee597190c9d23ad5e2b01ac
Omar <git@viciexperts.com>
2026-06-19 15:05:10 -0400

    Normalize SMTP mail scheme

--- config/mail.php
+++ config/mail.php
@@ -39,7 +39,11 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
+            'scheme' => match (env('MAIL_SCHEME')) {
+                'tls', 'starttls', 'smtp' => 'smtp',
+                'ssl', 'smtps' => 'smtps',
+                default => env('MAIL_SCHEME'),
+            },
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
             'port' => env('MAIL_PORT', 2525),
